86a46373b084ce5674860e74c60797320f86fa3f
Daniel Rubio <drubio1989@gmail.com>
2023-09-20 23:47:37 -0500

    Implement fetch posts and begin troubleshooting for posts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,38 @@
-export default function Home() {
+// import { fetchPosts } from "@/lib/actions/thread.actions";
+import User from "@/lib/models/user.model";
+import { currentUser } from "@clerk/nextjs";
+// import ThreadCard from "@/components/cards/ThreadCard";
+
+export default async function Home() {
+  // const result = await fetchPosts(1, 30);
+  const user = await currentUser();
+
   return (
     <>
       <h1 className='head-text text-left'>Home</h1>
+
+      {/* <section className='mt-9 flex flex-col gap-10'>
+        {result.postslength === 0 ? (
+          <p> No threads found</p>
+        ):(
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user?.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+       
+      </section> */}
     </>  
   )
 }
