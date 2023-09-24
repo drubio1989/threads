e3421c129d9034d56ed4039a74532c493eeb1c92
Daniel Rubio <drubio1989@gmail.com>
2023-09-24 16:53:01 -0500

    complete comment rendering display

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -15,7 +15,7 @@ const Page = async ({ params }: { params: { id: string }}) => {
   if (!userInfo.onboarded) return redirect('/onboarding');
 
   const thread = await fetchThreadById(params.id)
-
+  console.log('hello',thread.children)
   return (
     <section className='relative'>
         <div>
@@ -38,6 +38,25 @@ const Page = async ({ params }: { params: { id: string }}) => {
             currentUserImg={user.imageUrl}
             currentUserId={JSON.stringify(userInfo._id)}
           />
+        </div>
+
+        <div className='mt-10'>
+          {thread.children.map((childItem: any) => {
+             return(
+             <ThreadCard
+                key={childItem._id}
+                id={childItem._id}
+                currentUserId={user?.id}
+                parentId={childItem.parentId}
+                content={childItem.text}
+                author={childItem.author}
+                community={childItem.community}
+                createdAt={childItem.createdAt}
+                comments={childItem.children}
+                isComment
+              />
+             )
+          })}
         </div>
       </section>
   )
