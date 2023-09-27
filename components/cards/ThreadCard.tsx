9657ca63bb1143a767ea64238b238e3f96063620
Daniel Rubio <drubio1989@gmail.com>
2023-09-27 11:04:53 -0500

    Modify community creation

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { formatDateString } from '@/lib/utils';
 
 interface Props {
   id: string
@@ -36,57 +37,74 @@ function ThreadCard({
   comments,
   isComment,
 }: Props) {
+  console.log('Community iD:', community)
   return (
     <article className={`flex w-full flex-col rounded-xl ${isComment ? 'px-0 xs:px-7' : 'bg-dark-2 p-7'} `}>
       
       <div className='flex items-start justify-between'>
+        <div className='flex w-full flex-1 flex-row gap-4'>
+          <div className='flex flex-col items-center'>
+            <Link 
+              className='relative h-11 w-11'
+              href={`/profile/${author.id}`}>
+                <Image src={author.image} alt="Profile Image" fill className='cursor-pointer rounded-full'/>
+            </Link>
+            <div className='thread-card_bar'/>
+          </div>
 
-        <div className='flex flex-col items-center'>
-          <Link 
-            className='relative h-11 w-11'
-            href={`/profile/${author.id}`}>
-              <Image src={author.image} alt="Profile Image" fill className='cursor-pointer rounded-full'/>
-          </Link>
-          <div className='thread-card_bar'/>
-        </div>
+          <div className='flex w-full flex-col ml-3'>
+            <Link  
+              className='w-fit'
+              href={`/profile/${author.id}`}>
+              <h4 className='cursor-pointer text-base-semibold text-light-1'>
+              {author.name}</h4>
+            </Link>
 
-        <div className='flex w-full flex-col ml-3'>
-          <Link  
-            className='w-fit'
-            href={`/profile/${author.id}`}>
-             <h4 className='cursor-pointer text-base-semibold text-light-1'>
-            {author.name}</h4>
-          </Link>
-
-          <p className='mt-2 text-small-regular text-light-2'>{content}</p>
-        
-          <div className={`${isComment && 'mb-10'} mt-5 flex flex-col gap-3`}>
-            <div className='flex gap-3.5'>
-              <Image 
-                className='cursor-pointer object-contain'
-                src='/assets/heart-gray.svg' alt='heart' width={24} height={24} />
-              <Link href={`/thread/${id}`}>
+            <p className='mt-2 text-small-regular text-light-2'>{content}</p>
+          
+            <div className={`${isComment && 'mb-10'} mt-5 flex flex-col gap-3`}>
+              <div className='flex gap-3.5'>
                 <Image 
                   className='cursor-pointer object-contain'
-                  src='/assets/reply.svg' alt='reply' width={24} height={24} />
-              </Link>
-             <Image 
-                className='cursor-pointer object-contain'
-                src='/assets/repost.svg' alt='repost' width={24} height={24} />
-             <Image 
-                className='cursor-pointer object-contain'
-                src='/assets/share.svg' alt='share' width={24} height={24} />
-            </div>
+                  src='/assets/heart-gray.svg' alt='heart' width={24} height={24} />
+                <Link href={`/thread/${id}`}>
+                  <Image 
+                    className='cursor-pointer object-contain'
+                    src='/assets/reply.svg' alt='reply' width={24} height={24} />
+                </Link>
+              <Image 
+                  className='cursor-pointer object-contain'
+                  src='/assets/repost.svg' alt='repost' width={24} height={24} />
+              <Image 
+                  className='cursor-pointer object-contain'
+                  src='/assets/share.svg' alt='share' width={24} height={24} />
+              </div>
 
-            {isComment && comments.length > 0 && (
-              <Link href={`/thread/${id}`}>
-                <p className='mt-1 text-suble-medium text-gray-1'>{comments.length} replies</p>
-              </Link>
-            )}
+              {isComment && comments.length > 0 && (
+                <Link href={`/thread/${id}`}>
+                  <p className='mt-1 text-suble-medium text-gray-1'>{comments.length} replies</p>
+                </Link>
+              )}
+            </div>
           </div>
-        </div>
+          </div>
+        {!isComment && community && (
+          <Link 
+            href={`/communities/${community.id}`} 
+            className='mt-5 flex items-center'>
+          <p className='text-subtle-medium text-gray-1'>
+            {formatDateString(createdAt)}
+            - {community.name} Community
+          </p>
+
+          <Image src={community.image}
+            alt='Community Name' width={14} height={14}
+            className='ml-1 rounded-full object-cover' 
+          />
+          </Link>
+        )}
       </div>
-  
+             
     </article>
   )
 }
